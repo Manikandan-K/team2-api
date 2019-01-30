6bef6063717957c1367c49a59c1bd17a1526c068
shibi chakravarthi <shibicr93>
2019-01-30 16:22:18 +0530

    updated dml

--- src/main/resources/db/migration/V7__seed_movies.sql
+++ src/main/resources/db/migration/V7__seed_movies.sql
@@ -1,5 +1,5 @@
 
-insert into public."Language" (name) values ("Tamil");
+insert into public."Language" (name) values ('Tamil');
 insert into public."Movie" (name, experiences,runtime,"languageId") values ('Kabali', 'RDX, Dolby Atmos, SUB',180,1);
 insert into public."Movie" (name, experiences,runtime,"languageId") values ('Banjo', 'RDX 3D',180,1);
 insert into public."Movie" (name, experiences,runtime,"languageId") values ('Suicide Squad', 'Dolby Atmos, RDX 3D, SUB',180,1);
