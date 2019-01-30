f6e3426fbecca9f095f656c8107e003ed8cfe7b1
shibi chakravarthi <shibicr93>
2019-01-30 18:16:04 +0530

    updated dml

---
+++ src/main/resources/db/migration/V12__seed_movie_id.sql
@@ -0,0 +1,12 @@
+
+DELETE FROM public."Movie" where id != 9;
+
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (1, 'Kabali', NULL, 180, 'RDX, Dolby Atmos, SUB', '2019-01-10', NULL, NULL, NULL, 1);
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (2, 'Banjo', NULL, 180, 'RDX 3D', '2019-01-10', NULL, NULL, NULL, 1);
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (3, 'Suicide Squad', NULL, 180, 'Dolby Atmos, RDX 3D, SUB', '2019-01-10', NULL, NULL, NULL, 1);
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (4, 'Namadhu', NULL, 180, 'RDX, SUB', '2019-01-10', NULL, NULL, NULL, 1);
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (5, 'Pelli Chupulu', NULL, 180, 'RDX, SUB', '2019-02-10', NULL, NULL, NULL, 1);
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (6, 'Pink', NULL, 180, 'RDX, SUB', '2019-02-10', NULL, NULL, NULL, 1);
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (7, 'Remo', NULL, 180, 'Dolby Atmos, RDX, SUB', '2019-02-10', NULL, NULL, NULL, 1);
+INSERT INTO public."Movie" (id, name, synopsis, runtime, experiences, "releaseDate", "cast", crew, "bannerImageUrl", "languageId") VALUES (8, 'Inferno', NULL, 180, 'Dolby Atmos, RDX, SUB', '2019-02-10', NULL, NULL, NULL, 1);
+
